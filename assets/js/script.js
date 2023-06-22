722db3ec5eb6833499a59f047736f44241faeebb
Marc-Hanson <>
2023-06-22 11:17:42 +0000

    Javascript function for computers turn.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('STOP MESSING WITH HTML AND CSS, WRITE ME!!');
+const options = ["fire", "grass", "rock", "ice", "ground"];
+const defaultHtml = document.getElementById('your-choice').textContent;
+
+//Generate a random number between 0 and 4 for computers turn and uses it to select a value from the 'options' array.
+function getJerryChoice(){
+    let jerryMath = options[Math.floor(Math.random() * options.length)];
+    let jerryChoice = document.getElementById(jerryMath).textContent;
+    document.getElementById('jerry-choice').innerHTML = jerryChoice;
+}
+
+//Returns player and computer choice to ? emoji after the game.
+function returnToDefault(){
+    document.getElementById('your-choice').innerHTML = defaultHtml;
+    document.getElementById('jerry-choice').innerHTML = defaultHtml;
+}
+getJerryChoice();
